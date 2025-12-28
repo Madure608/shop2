968c7b5cea33448f90370adc53f604fd29ca3517
Madure608 <maduranga7395@gmail.com>
2025-12-28 20:02:58 +0530

    update productController.js file

---
+++ mern course/backend/controllers/productController.js
@@ -0,0 +1,22 @@
+import product from "../models/product.js";
+export async function createProduct(req,res) {
+
+    const product = new product(req.body)
+
+    try{
+        const response = await product.save();
+
+        res.json({
+            message : "Product created successfully",
+            product: response
+        })
+
+    }catch(error) {
+        console.error("Error creating product:", error);
+        return res.status(500).json({ message: "Failed to create product"})
+
+
+    }
+    
+    
+}
