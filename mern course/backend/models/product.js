9c065311868c47327206cc06c88f48de1af60bad
Madure608 <maduranga7395@gmail.com>
2025-12-28 19:27:09 +0530

    update product.js file

--- mern course/backend/models/product.js
+++ mern course/backend/models/product.js
@@ -6,6 +6,44 @@ const productSchema = new mongoose.Schema({
         required : true,
         unique : true
     },
-
+    name : {
+        type : String,
+        required : true
+    },
+    altNames : {
+        type : [String],
+        default : []
+    },
+    labelledPrice : {
+        type : Number,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    images : {
+        type : [String],
+        default : ["/default-product.jpg"]
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true,
+        default : 0
+    },
+    isAvailable : {
+        type : Boolean,
+        default : true
+    },
+    category : {
+        type : String,
+        required : true,
+        default : "cosmatics"
     }
+    
+    
 })
