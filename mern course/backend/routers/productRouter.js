3a5c7490c97c5fea0f888d7e2f7b916afb2ebcc1
Madure608 <maduranga7395@gmail.com>
2026-01-01 21:11:47 +0530

    update productRouter.js file

--- mern course/backend/routers/productRouter.js
+++ mern course/backend/routers/productRouter.js
@@ -5,6 +5,9 @@ import { createProduct, getProducts } from '../controllers/productController.js'
 const productRouter = express.Router();
 productRouter.post("/", createProduct)
 productRouter.get("/", getProducts)
+productRouter.delete("/:productId", (req,res)=>{
+    console.log("Delete request triggered")
+})
 
 export default productRouter;
 
